9045944d826eb5469cff15540a47a4ba6f1e9451
MD Khairuzzaman Rakib <mdkhairuzzamanrakib153@gmail.com>
2023-09-27 22:49:29 +0600

    Models Created

---
+++ MVC Project/Models/DbModels.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Web;
+
+namespace MVC_Project.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        [Required, StringLength(40), Display(Name ="Product Name")]
+        public string ProductName { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal Discount { get; set; }
+    }
+}
